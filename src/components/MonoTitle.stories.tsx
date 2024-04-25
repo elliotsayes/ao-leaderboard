e3c5ae34ec959294864e7fec673823b39e351fcb
Elliot Sayes <elliot@sayes.cloud>
2024-04-25 13:49:36 +0800

    Fix MonoTitle

--- src/components/MonoTitle.stories.tsx
+++ src/components/MonoTitle.stories.tsx
@@ -30,6 +30,6 @@ export const Default: Story = {}
 
 export const ExpLeaderboard: Story = {
   args: {
-    children: "EXP Leaderboard",
+    children: "EXP LEADERBOARD",
   },
 }
--- src/components/MonoTitle.tsx
+++ src/components/MonoTitle.tsx
@@ -2,16 +2,14 @@ import { cn } from "@/lib/utils"
 import React from "react"
 
 export interface MonoTitleProps
-  extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
+  extends React.HTMLAttributes<HTMLParagraphElement> {
 }
 
-const MonoTitle = React.forwardRef<HTMLAnchorElement, MonoTitleProps>(
+const MonoTitle = React.forwardRef<HTMLParagraphElement, MonoTitleProps>(
   ({ className, ...props }, ref) => {
     return (
-      <a
-        className={cn("font-mono font-[400] text-[14px] leading-[18.2px] text-title capitalize", className)}
-        target="_blank"
-        referrerPolicy="no-referrer"
+      <p
+        className={cn("font-mono font-[400] text-[14px] leading-[18.2px] text-title", className)}
         ref={ref}
         {...props}
       />
