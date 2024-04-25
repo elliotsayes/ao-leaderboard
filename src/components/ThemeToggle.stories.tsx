9cec9795dbdf6ae0bc85590fadd190849f0991b0
Elliot Sayes <elliot@sayes.cloud>
2024-04-25 18:07:26 +0800

    Fix category

--- src/components/ThemeToggle.stories.tsx
+++ src/components/ThemeToggle.stories.tsx
@@ -8,7 +8,7 @@ import React from "react";
  * ThemeToggle container
  */
 const meta = {
-  title: "_Lucas/layout/ThemeToggle",
+  title: "_Lucas/buttons/ThemeToggle",
   component: ThemeToggle,
   tags: ["autodocs"],
   parameters: {
