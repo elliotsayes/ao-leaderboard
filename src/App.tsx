4ecdcc15971867aaf1fd8d968f466e3eff57ccea
Elliot Sayes <elliot@sayes.cloud>
2024-04-29 12:01:04 +0900

    Loading from query

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
-// import ContractData from "../fixtures/leaderboards/contract_gAC5hpUPh1v-oPJLnK3Km6-atrYlvI271bI-q0yZOnw.json"
-// import Gen100Data from "../fixtures/leaderboards/gen_100.json"
-import Gen100KData from "../fixtures/leaderboards/gen_100K.json"
-// import Gen1MData from "../fixtures/leaderboards/gen_1M.json"
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/query";
 
-import { TableVirtualizedInfiniteProp } from "./components/TableVirtualizedInfiniteProp"
-
-const storyData = {
-  // "default": ContractData,
-  // "100": Gen100Data,
-  "100K": Gen100KData,
-  // "1M": Gen1MData,
-};
+import { TestN } from "./TestN"
 
 function App() {
   return (
-    <>
-      <TableVirtualizedInfiniteProp flatData={storyData["100K"]} />
-    </>
+    <QueryClientProvider client={queryClient}>
+      <TestN />
+    </QueryClientProvider>
   )
 }
 
---
+++ src/TestN.tsx
@@ -0,0 +1,32 @@
+import { useQuery } from "@tanstack/react-query";
+import { TableVirtualizedInfiniteProp } from "./components/TableVirtualizedInfiniteProp";
+import { useState } from "react";
+
+const options = ["100", "100K", "1M"] as const;
+
+export const TestN = () => {
+  const [option, setOption] = useState<typeof options[number]>(options[0]);
+
+  const { data } = useQuery({
+    queryKey: [`test${option}`],
+    queryFn: async () => {
+      const response = await fetch(`./gen_${option}.json`)
+      const data = await response.json()
+      return data
+    },
+  })
+
+  if (!data) return <div>Loading...</div>
+
+  return (
+    <>
+      <h1>Test N</h1>
+      <select value={option} onChange={(e) => setOption(e.target.value as typeof options[number])}>
+        {options.map((o) => (
+          <option key={o} value={o}>{o}</option>
+        ))}
+      </select>
+      <TableVirtualizedInfiniteProp flatData={data} />
+    </>
+  )
+}
