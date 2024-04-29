483d1063ac7c7664e42ff43744b267054f6933fb
Elliot Sayes <elliot@sayes.cloud>
2024-04-29 19:32:59 +0900

    Style the SearchBar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,19 +1,49 @@
+import {
+  MagnifyingGlassIcon,
+  Cross2Icon
+} from "@radix-ui/react-icons"
+import { useRef } from "react";
+
 interface SearchBarProps {
   value: string;
   onChange: (value: string) => void;
 }
 
 export const SearchBar = ({ value, onChange }: SearchBarProps) => {
+  const inputRef = useRef<HTMLInputElement>(null)
+  const hasText = value.length > 0
+
   return (
-    <div className="flex items-center px-2 text-nowrap">
+    <div className="group flex flex-row items-center">
       <input
+        ref={inputRef}
+        className={`w-full h-full px-3 py-2 text-sm bg-transparent outline-none border-transparent border-solid border-2 rounded-sm group-hover:outline-section-border/10 ${hasText ? "border-section-border/40" : "focus:border-section-border/20"}`}
         type="text"
-        className="w-full px-2 py-1 text-sm text-gray-700 placeholder-gray-400 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:placeholder-gray-500 dark:border
-        dark:focus:ring-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:border-blue-300"
-        placeholder="Search"
         value={value}
         onChange={(e) => onChange(e.target.value)}
       />
+      <div
+        className="relative pl-2"
+        onClick={() => inputRef.current?.focus()}
+      >
+        <MagnifyingGlassIcon
+          width={36}
+          height={36}
+          className="p-1 text-section-text group-hover:text-section-text/10"
+        />
+        {hasText && (
+          <div
+            className="absolute top-0 right-0 z-10 bg-section-background/10 rounded-sm cursor-pointer"
+            onClick={() => onChange("")}
+          >
+            <Cross2Icon
+              width={16}
+              height={16}
+              className="text-red-600"
+            />
+          </div>
+        )}
+      </div>
     </div>
   )
 }
