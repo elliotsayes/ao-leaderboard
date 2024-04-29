f1cf098734f3268ed7d6b302a7282ae5fee8f1d1
Elliot Sayes <elliot@sayes.cloud>
2024-04-30 08:01:29 +0900

    Hover underline links

--- src/components/MonoLink.tsx
+++ src/components/MonoLink.tsx
@@ -9,7 +9,7 @@ const MonoLink = React.forwardRef<HTMLAnchorElement, MonoLinkProps>(
   ({ className, ...props }, ref) => {
     return (
       <a
-        className={cn("font-mono font-[500] text-[12px] leading-[15.6px] text-title", className)}
+        className={cn("font-mono font-[500] text-[12px] leading-[15.6px] text-title hover:underline", className)}
         target="_blank"
         referrerPolicy="no-referrer"
         ref={ref}
