ac0efdcc720880afd73017ed9567f5fb336f6107
Elliot Sayes <elliot@sayes.cloud>
2024-04-29 11:28:17 +0900

    Generate leaderboards

--- scripts/generate.ts
+++ scripts/generate.ts
@@ -1,9 +1,18 @@
+import { file } from "bun";
 import { generateBalances } from "./generateBalances";
+import { generateLeaderboards } from "./generateLeaderboards";
 
 async function generate() {
-  await generateBalances(100, "fixtures/balances/gen_100.json")
-  await generateBalances(100_000, "fixtures/balances/gen_100K.json")
-  await generateBalances(1_000_000, "fixtures/balances/gen_1M.json")
+  const balancesContract = await file("fixtures/balances/contract_gAC5hpUPh1v-oPJLnK3Km6-atrYlvI271bI-q0yZOnw.json").json() as BalancesRaw
+  await generateLeaderboards(balancesContract, "fixtures/leaderboards/contract_gAC5hpUPh1v-oPJLnK3Km6-atrYlvI271bI-q0yZOnw.json")
+
+  const balances100 = await generateBalances(100, "fixtures/balances/gen_100.json")
+  const balances100K = await generateBalances(100_000, "fixtures/balances/gen_100K.json")
+  const balances1M = await generateBalances(1_000_000, "fixtures/balances/gen_1M.json")
+
+  await generateLeaderboards(balances100, "fixtures/leaderboards/gen_100.json")
+  await generateLeaderboards(balances100K, "fixtures/leaderboards/gen_100K.json")
+  await generateLeaderboards(balances1M, "fixtures/leaderboards/gen_1M.json")
 }
 
 generate();
---
+++ scripts/generateLeaderboards.ts
@@ -0,0 +1,12 @@
+
+import type { BalancesRaw } from "../src/lib/ao/balances"
+import { balancesRawToFlat } from "../src/lib/model/table"
+
+export async function generateLeaderboards(balances: BalancesRaw, outputPath: string): Promise<LeaderBoardFlat> {
+  const leaderboardFlat = balancesRawToFlat(balances)
+
+  const leaderboardFlatJson = JSON.stringify(leaderboardFlat)
+  await Bun.write(outputPath, leaderboardFlatJson)
+  
+  return balances
+}
