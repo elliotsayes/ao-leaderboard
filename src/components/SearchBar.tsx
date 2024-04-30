c62df8ed3a5007fe6f381598c424d8f5dfc8e022
Elliot Sayes <elliot@sayes.cloud>
2024-04-30 17:31:24 +0900

    Restyle searchbar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,8 +1,6 @@
 import {
   MagnifyingGlassIcon,
-  Cross2Icon
 } from "@radix-ui/react-icons"
-import { useRef } from "react";
 
 interface SearchBarProps {
   value: string;
@@ -10,40 +8,22 @@ interface SearchBarProps {
 }
 
 export const SearchBar = ({ value, onChange }: SearchBarProps) => {
-  const inputRef = useRef<HTMLInputElement>(null)
   const hasText = value.length > 0
 
   return (
-    <div className="group flex flex-row items-center">
+    <div className="group flex flex-row items-center relative">
       <input
-        ref={inputRef}
-        className={`w-full h-full px-3 py-2 text-sm bg-transparent outline-none border-solid border-2 rounded-sm group-hover:outline-section-border/10 ${hasText ? "border-section-border/40" : "border-transparent focus:border-section-border/20"}`}
+        className={`w-full h-full pl-3 pr-10 py-2 text-sm bg-transparent outline-none border-solid border-2 rounded-sm group-hover:outline-section-border/10 ${hasText ? "border-section-border/40" : "border-transparent focus:border-section-border/20"}`}
         type="text"
         value={value}
         onChange={(e) => onChange(e.target.value)}
+        placeholder="Search Wallet"
       />
-      <div
-        className="relative pl-2"
-        onClick={() => inputRef.current?.focus()}
-      >
-        <MagnifyingGlassIcon
-          width={36}
-          height={36}
-          className="p-1 text-section-text group-hover:text-section-text/10"
-        />
-        {hasText && (
-          <div
-            className="absolute top-0 right-0 p-0.5 z-10 bg-section-background/10 rounded-sm cursor-pointer"
-            onClick={() => onChange("")}
-          >
-            <Cross2Icon
-              width={16}
-              height={16}
-              className="text-red-600"
-            />
-          </div>
-        )}
-      </div>
+      <MagnifyingGlassIcon
+        width={32}
+        height={32}
+        className="absolute right-1 z-10 my-auto text-section-text group-hover:text-section-text/10 pointer-events-none"
+      />
     </div>
   )
 }
