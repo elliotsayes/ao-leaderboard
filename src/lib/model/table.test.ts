74fbcf306b2da81b57180b5f24178d9c232b3588
Elliot Sayes <elliot@sayes.cloud>
2024-05-23 00:00:01 +0900

    Denomination test

--- src/lib/model/table.test.ts
+++ src/lib/model/table.test.ts
@@ -4,16 +4,16 @@ import { balancesRawToFlat } from './table';
 describe('balancesRawToFlat', () => {
   it('should convert balancesRaw to LeaderBoardFlat', () => {
     const balancesRaw = {
-      'address1': 1,
-      'address2': 2,
-      'address3': 3,
+      'address1': 11,
+      'address2': 22,
+      'address3': 33,
     };
     const expected = [
-      { rank: 1, address: 'address3', score: 3 },
-      { rank: 2, address: 'address2', score: 2 },
-      { rank: 3, address: 'address1', score: 1 },
+      { rank: 1, address: 'address3', score: 3.3 },
+      { rank: 2, address: 'address2', score: 2.2 },
+      { rank: 3, address: 'address1', score: 1.1 },
     ];
-    const actual = balancesRawToFlat(balancesRaw, 0);
+    const actual = balancesRawToFlat(balancesRaw, 1);
     expect(actual).toEqual(expected);
   });
 });
